96175478156c62ba05e0cfe65f057eae3d058030
wans <1073485249@qq.com>
2017-05-15 12:54:05 +0800

    修改页面跳转问题

--- src/main/java/com/wanyu/learn/action/UserController.java
+++ src/main/java/com/wanyu/learn/action/UserController.java
@@ -1,30 +1,60 @@
 package com.wanyu.learn.action;
 
-import com.wanyu.learn.domain.UserT;
-import com.wanyu.learn.service.IUserTService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.wanyu.learn.dao.UserMapper;
+import com.wanyu.learn.domain.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+
 
 
 /**
  * Created by Administrator on 2017/4/28.
  */
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/shiro")
 public class UserController {
+   @Resource
+   private UserMapper  userMapper;
 
-    @Autowired
-    private IUserTService userTService;
-    @RequestMapping("/showUser")
-    public String toIndex(HttpServletRequest request,Model model){
-              int id = Integer.parseInt(request.getParameter("id"));
-              UserT user  = this.userTService.getUserTById(id);
-              model.addAttribute("user",user);
-              model.addAttribute("complain","the fucking SSM go shit");
-              return "showUser";
+   @RequestMapping("/loginAdmin")
+   public String login(User user,Model model){
+          Subject subject = SecurityUtils.getSubject();
+          UsernamePasswordToken  usernamePasswordToken =
+                  new UsernamePasswordToken(user.getUsername(),user.getPassword());
+          try{
+              subject.login(usernamePasswordToken);
+              /*
+                 这里不能直接return "admin" 因为SpringMVC的配置  前缀是"/"
+                 它会寻找同级别的目录。导致出问题，实际上资源所在的目录是/WEB-INF/jsp/admin.jsp
+               */
+              return "WEB-INF/jsp/admin";
+          }catch (Exception ex){
+              model.addAttribute("error","用户名或密码错误");
+              /*
+               * 这里不能直接使用 return "login"理论上是可以跳转到页面，然后一切正常，
+               * 但实际会产生的问题是jq的静态资源会被拦截，导致页面无法加载脚本
+               */
+              return "redirect:/login";
+          }
+
+   }
+    @RequestMapping("/admin")
+    public String admin(){
+        return "WEB-INF/jsp/admin";
+    }
+
+    @RequestMapping("/student")
+    public String student(){
+        return "WEB-INF/jsp/admin";
+    }
+
+    @RequestMapping("/teacher")
+    public String teacher(){
+        return "WEB-INF/jsp/admin";
     }
 }
