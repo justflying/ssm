43b7a598b12db887b49ca6ce73ab99b29baf98d6
wans <1073485249@qq.com>
2017-05-23 13:39:23 +0800

    整合shiro

--- src/main/java/com/wanyu/learn/service/IUserTService.java
+++ src/main/java/com/wanyu/learn/service/IUserTService.java
@@ -1,10 +1,14 @@
 package com.wanyu.learn.service;
 
-import com.wanyu.learn.domain.UserT;
+import com.wanyu.learn.domain.User;
+
+import java.util.Set;
 
 /**
  * Created by Administrator on 2017/4/28.
  */
 public interface IUserTService {
-    UserT getUserTById(int id);
+   User findUserByUsername(String username);
+   Set<String> findRoles(String username);
+   Set<String> findPermissions(String username);
 }
