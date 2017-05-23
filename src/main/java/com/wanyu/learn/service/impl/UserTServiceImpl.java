eac70d0f0a38ea6ce88006d9044f4ae32b3179aa
wans <1073485249@qq.com>
2017-05-23 13:39:12 +0800

    添加方法和实现

--- src/main/java/com/wanyu/learn/service/impl/UserTServiceImpl.java
+++ src/main/java/com/wanyu/learn/service/impl/UserTServiceImpl.java
@@ -1,24 +1,32 @@
 package com.wanyu.learn.service.impl;
 
-import com.wanyu.learn.dao.UserTMapper;
-import com.wanyu.learn.domain.UserT;
+import com.wanyu.learn.dao.UserMapper;
+import com.wanyu.learn.domain.User;
 import com.wanyu.learn.service.IUserTService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Set;
+
 
 /**
  * Created by Administrator on 2017/4/28.
  */
 
-@Service("userTService")
-public class UserTServiceImpl implements IUserTService {
-
+@Service
+public class UserTServiceImpl  implements IUserTService{
     @Resource
-    private UserTMapper userTMapper;
+    private UserMapper userMapper;
 
-    public UserT getUserTById(int id) {
-        UserT userT =userTMapper.selectByPrimaryKey(id);
-        return userT;
+    public User findUserByUsername(String username) {
+        return userMapper.findUserByUsername(username);
+    }
+
+    public Set<String> findRoles(String username) {
+        return userMapper.findRoles(username);
+    }
+
+    public Set<String> findPermissions(String username) {
+        return userMapper.findPermissions(username);
     }
 }
